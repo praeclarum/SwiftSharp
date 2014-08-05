7f783a82551cf1dbc45b00a2dfd32577ef83f1f4
Frank A. Krueger <fak@praeclarum.org>
2014-08-04 22:16:51 -0700

    More work on classes and structs

--- SwiftSharp/SwiftParser.fs
+++ SwiftSharp/SwiftParser.fs
@@ -20,11 +20,13 @@ type Statement =
 
 and Declaration =
     | ImportDeclaration of string list
-    | VariableDeclaration of (Pattern * (Expression option)) list
+    | GetterSetterVariableDeclaration of (string list) * (string * Type) * ((Statement list) * ((Statement list) option))
+    | PatternVariableDeclaration of (string list) * ((Pattern * (Expression option)) list)
     | ConstantDeclaration of (Pattern * (Expression option)) list
     | TypealiasDeclaration of string * Type
     | StructDeclaration of string * (Type list) * (Declaration list)
-    | InitializerDeclaration of string list
+    | ClassDeclaration of string * (Type list) * (Declaration list)
+    | InitializerDeclaration of ((string * (string option) * Type) list) * (Statement list)
 
 and Expression =
     | Number of float
@@ -35,6 +37,10 @@ and Expression =
 
 and Binary = string * Expression
 
+
+//
+// Lexer
+//
 
 type Document =
     {
@@ -52,11 +58,7 @@ type Position =
     member this.Eof = this.Index >= this.Document.Body.Length
     member this.PreviousText =
         let n = Math.Min (this.Index, 32)
-        this.Document.Body.Substring (this.Index - n, n)
-
-//
-// Lexing
-//
+        this.Document.Body.Substring (this.Index - n, n + 1)
 
 let rec ws (i : Position) : Position =
     if i.Eof then i
@@ -119,6 +121,14 @@ let (|String|) (i : Position) =
             Some (body.Substring (i.Index + 1, e - i.Index - 1), i.Advance (e - i.Index + 1))
         else None
 
+let (|Newline|) p1 =
+    let b = p1.Document.Body
+    let n = b.Length
+    let p2 = ws p1
+    match b.IndexOf ('\n', p1.Index, p2.Index - p1.Index) with
+    | x when x < 0 -> None
+    | _ -> Some p2
+
 let (|Token|) (text : string) i =
     let b = i.Document.Body
     let n = b.Length
@@ -127,6 +137,11 @@ let (|Token|) (text : string) i =
         match String.CompareOrdinal (text, 0, b, i.Index, text.Length) with
         | 0 -> Some (i.Advance text.Length)
         | _ -> None
+
+let (|TokenValue|) (text : string) p1 =
+    match p1 with
+    | Token text (Some p2) -> Some (text, p2)
+    | _ -> None
 
 let isNum c = Char.IsDigit (c) || c = '.'
 
@@ -153,6 +168,11 @@ let (|Binary_operator|) (i : Position) =
 // Combinators
 //
 
+let opt pattern p1 =
+    match pattern p1 with
+    | Some (v1, p2) -> Some (Some v1, p2)
+    | _ -> Some (None, p1)
+
 let rec oneOrMore pattern p1 =
     match pattern p1 with
     | Some (v1, p2) ->
@@ -160,6 +180,14 @@ let rec oneOrMore pattern p1 =
         | Some (v2, p3) -> Some (v1 :: v2, p3)
         | _ -> Some ([v1], p2)
     | _ -> None
+
+let rec zeroOrMore pattern p1 =
+    match pattern p1 with
+    | Some (v1, p2) ->
+        match zeroOrMore pattern (ws p2) with
+        | Some (v2, p3) -> Some (v1 :: v2, p3)
+        | _ -> Some ([v1], p2)
+    | _ -> Some ([], p1)
 
 let rec oneOrMoreSep pattern sep p1 =
     match pattern p1 with
@@ -170,6 +198,22 @@ let rec oneOrMoreSep pattern sep p1 =
             | Some (v2, p3) -> Some (v1 :: v2, p3)
             | _ -> None
         | _ -> Some ([v1], p2)
+    | _ -> None
+
+let (|||) x y p1 =
+    match x p1 with
+    | Some (v1, p2) -> Some (v1, p2)
+    | _ ->
+        match y p1 with
+        | Some (v1, p2) -> Some (v1, p2)
+        | _ -> None
+
+let (&&&) x y p1 =
+    match x p1 with
+    | Some (v1, p2) ->
+        match y (ws p2) with
+        | Some (v2, p3) -> Some ((v1, v2), p3)
+        | _ -> None
     | _ -> None
 
 
@@ -300,12 +344,12 @@ and (|Binary_expressions|) i =
         | _ -> Some ([be], j)
     | _ -> None
 
-let rec (|Pattern|) i =
-    match i with
-    | Identifier_pattern (Some (y, j)) ->
-        match ws j with
-        | Type_annotation (Some (z, k)) -> Some (IdentifierPattern (y, Some z), k)
-        | _ -> Some (IdentifierPattern (y, None), j)
+let rec (|Pattern|) p1 =
+    match p1 with
+    | Identifier_pattern (Some (v1, p2)) ->
+        match ws p2 with
+        | Type_annotation (Some (v2, p3)) -> Some (IdentifierPattern (v1, Some v2), p3)
+        | _ -> Some (IdentifierPattern (v1, None), p2)
     | _ -> None
 
 and (|Identifier_pattern|) i =
@@ -326,9 +370,69 @@ and (|Declaration|) p1 =
     | Variable_declaration (Some r) -> Some r
     | Typealias_declaration (Some r) -> Some r
     | Struct_declaration (Some r) -> Some r
-//    | Initializer_declaration (Some r) -> Some r
+    | Class_declaration (Some r) -> Some r
+    | Initializer_declaration (Some r) -> Some r
     | _ -> None
 
+//initializer-declaration → initializer-head generic-parameter-clause_opt parameter-clause initializer-body
+//initializer-head → attributes_opt "convenience"_opt "init"
+//initializer-body → code-block
+and (|Initializer_declaration|) p1 =
+    match p1 with
+    | Initializer_head (Some (v1, p2)) ->
+        match ws p2 with
+        | Parameter_clause (Some (v2, p3)) ->
+            match p3 with // Don't call ws before code block
+            | Code_block (Some (v3, p4)) -> Some (InitializerDeclaration (v2, v3), p4)
+            | _ -> None
+        | _ -> None
+    | _ -> None
+
+and (|Initializer_head|) p1 =
+    match p1 with
+    | Token "init" (Some p2) -> Some (0, p2)
+    | _ -> None
+
+and (|Parameter_name|) = (|Identifier|) ||| ((|TokenValue|) "_")
+
+and (|Local_parameter_name|) = (|Identifier|) ||| ((|TokenValue|) "_")
+
+and (|Parameter|) p1 =
+    match p1 with
+    | Parameter_name (Some (v1, p2)) ->
+        match ws p2 with
+        | Local_parameter_name (Some (v2, p3)) ->
+            match ws p3 with
+            | Type_annotation (Some (v3, p4)) -> Some ((v1, Some v2, v3), p4)
+            | _ -> None
+        | Type_annotation (Some (v2, p3)) -> Some ((v1, None, v2), p3)
+        | _ -> None
+    | _ -> None
+
+and (|Parameter_list|) = oneOrMoreSep (|Parameter|) ","
+
+and (|Parameter_clause|) p1 =
+    match p1 with
+    | Token "(" (Some p2) ->
+        match ws p2 with
+        | Token ")" (Some p3) -> Some ([], p3)
+        | Parameter_list (Some (v2, p3)) ->
+            match ws p3 with
+            | Token ")" (Some p4) -> Some (v2, p4)
+            | _ -> None
+        | _ -> None
+    | _ -> None
+
+and (|Code_block|) p1 =
+    match ws p1 with
+    | Token "{" (Some p2) ->
+        match ws p2 with
+        | Token "}" (Some p3) -> Some ([], p3)
+        | _ -> None
+    | _ ->
+        match p1 with
+        | Newline (Some p2) -> Some ([], p2)
+        | _ -> None
 
 
 //struct_declaration
@@ -349,14 +453,31 @@ and (|Struct_declaration|) i =
             match ws p3 with
             | Type_inheritance_clause (Some (v3, p4)) ->
                 match ws p4 with
-                | Struct_body (Some (v4, p5)) -> Some (StructDeclaration (v2, v3, v4), p5)
+                | Class_or_struct_body (Some (v4, p5)) -> Some (StructDeclaration (v2, v3, v4), p5)
                 | _ -> None
-            | Struct_body (Some (v3, p4)) -> Some (StructDeclaration (v2, [], v3), p4)
+            | Class_or_struct_body (Some (v3, p4)) -> Some (StructDeclaration (v2, [], v3), p4)
             | _ -> None
         | _ -> None
     | _ -> None
 
 and (|Struct_name|) = (|Identifier|)
+
+and (|Class_declaration|) i =
+    match i with
+    | Token "class" (Some p2) ->
+        match ws p2 with
+        | Class_name (Some (v2, p3)) ->
+            match ws p3 with
+            | Type_inheritance_clause (Some (v3, p4)) ->
+                match ws p4 with
+                | Class_or_struct_body (Some (v4, p5)) -> Some (ClassDeclaration (v2, v3, v4), p5)
+                | _ -> None
+            | Class_or_struct_body (Some (v3, p4)) -> Some (ClassDeclaration (v2, [], v3), p4)
+            | _ -> None
+        | _ -> None
+    | _ -> None
+
+and (|Class_name|) = (|Identifier|)
 
 and (|Type_inheritance_clause|) p1 =
     match p1 with
@@ -368,12 +489,15 @@ and (|Type_inheritance_clause|) p1 =
 
 and (|Type_inheritance_list|) = oneOrMoreSep (|Type_identifier|) ","
 
-and (|Struct_body|) p1 =
+and (|Class_or_struct_body|) p1 =
     match p1 with
     | Token "{" (Some p2) ->
         match ws p2 with
         | Token "}" (Some p3) -> Some ([], p3)
-        | Declarations (Some n2) -> Some n2
+        | Declarations (Some (v2, p3)) ->
+            match ws p3 with
+            | Token "}" (Some p4) -> Some (v2, p4)
+            | _ -> None
         | _ -> None
     | _ -> None
 
@@ -404,16 +528,59 @@ and (|Constant_declaration|) i =
         | _ -> None
     | _ -> None
 
-and (|Variable_declaration_head|) i =
-    match i with
-    | Token "var" (Some j) -> Some j
+and (|Declaration_specifier|) = ((|TokenValue|) "static") ||| ((|TokenValue|) "override")
+
+and (|Declaration_specifiers|) = oneOrMore (|Declaration_specifier|)
+
+and (|Declaration_specifiers_opt|) = zeroOrMore (|Declaration_specifier|)
+
+and (|Variable_declaration_head|) = (|Declaration_specifiers_opt|) &&& ((|TokenValue|) "var")
+
+and (|Single_variable_declaration|) specs p1 =
+    match p1 with
+    | Identifier (Some (v1, p2)) ->
+        match ws p2 with
+        | Type_annotation (Some (v2, p3)) ->
+            match ws p3 with
+            | Getter_setter_block (Some (v3, p4)) -> Some (GetterSetterVariableDeclaration (specs, (v1, v2), v3), p4)
+            | _ -> None
+        | _ -> None
+    | _ -> None
+        
+and (|Variable_declaration|) p1 =
+    match p1 with
+    | Variable_declaration_head (Some ((v1, _), p2)) ->
+        match ws p2 with
+        | Single_variable_declaration v1 (Some n2) -> Some n2
+        | Pattern_initializer_list (Some (v2, p3)) -> Some (PatternVariableDeclaration (v1, v2), p3)
+        | _ -> None
     | _ -> None
 
-and (|Variable_declaration|) i =
-    match i with
-    | Variable_declaration_head (Some j) ->
-        match ws j with
-        | Pattern_initializer_list (Some (z, k)) -> Some (VariableDeclaration z, k)            
+and (|Getter_clause|) p1 =
+    match p1 with
+    | Token "get" (Some p2) ->
+        match p2 with
+        | Code_block (Some (v2, p3)) -> Some (v2, p3)
+        | _ -> Some ([], p2)
+    | _ -> None
+
+and (|Setter_clause|) p1 =
+    match p1 with
+    | Token "set" (Some p2) ->
+        match p2 with
+        | Code_block (Some (v2, p3)) -> Some (v2, p3)
+        | _ -> Some ([], p2)
+    | _ -> None
+
+and (|Getter_setter_block|) p1 =
+    match p1 with
+    | Token "{" (Some p2) ->
+        match ws p2 with
+        | Getter_clause (Some (v2, p3)) ->
+            match ws p3 with
+            | Setter_clause (Some (v3, p4)) -> Some ((v2, Some v3), p4)
+            | Token "}" (Some p4) -> Some ((v2, None), p4)
+            | _ -> None
         | _ -> None
     | _ -> None
 
